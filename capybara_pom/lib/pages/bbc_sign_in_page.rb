21e049359a5136d20056c26782fcccf6d3c35968
Tech-A12 <tech-a12@Tech-A12s-MacBook-Air.local>
2018-06-14 17:14:44 +0100

    Wrote some comments

--- capybara_pom/lib/pages/bbc_sign_in_page.rb
+++ capybara_pom/lib/pages/bbc_sign_in_page.rb
@@ -1,4 +1,6 @@
 require 'capybara/dsl'
+
+#Make the generator available
 require_relative '../generator/generator'
 
 
@@ -12,7 +14,11 @@ class BbcSignInPage
   USERNAME_FIELD = 'user-identifier-input'
 
 
-
+#need to instantiate the GENERATOR superclass in order to use either generator.
+#Here we'vedone that and called on it the method random_form_values which opens the
+#door to the methods contained inside the random form values gneerator.
+#So now we can use the methods that generate
+# the info to fill in the form e.g generate_first_name
   def fill_username_field
     @random_name = Generator.new.random_form_values.generate_first_name
     fill_in(USERNAME_FIELD, with: "#{@random_name}")
